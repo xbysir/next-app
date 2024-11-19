fd29b78949f783df14acf220e9e00941991e5fec
方海涛 <fanghaitao@jianzhikeji.com>
2024-11-19 15:47:10 +0800

    feat: 修改文案

--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,10 @@ export default function Home() {
         This is just the beginning of something amazing.
       </p>
       <Link href="/about" className="text-blue-500 hover:underline">
-        About Us 页面
+        跳转About Us 页面
       </Link>
       <Link href="/posts" className="text-green-500 hover:underline">
-        posts 页面
+        跳转posts 页面
       </Link>
     </main>
   );
