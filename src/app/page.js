7faf9926673a348c0092db278d7f1fe65a7e97be
xby <2549621945@qq.com>
2024-11-19 13:21:10 +0800

    feat: 跳转页面

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+import Link from 'next/link';
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      首页
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to My First Next.js App!</h1>
+      <p className="text-xl">
+        This is just the beginning of something amazing.
+      </p>
+      <Link href="/about" className="text-blue-500 hover:underline">
+        About Us
+      </Link>
+    </main>
   );
 }
